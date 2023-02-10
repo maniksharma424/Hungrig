6a299a6ebc3c4ad46255ac7b74b1ad7f5fa2b99d
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-02-10 16:30:53 +0530

    5

--- dist/index.c3f022c4.js
+++ dist/index.c3f022c4.js
@@ -34682,7 +34682,7 @@ $RefreshReg$(_c, "Cart");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","./CheckOutBox":"4TIou","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"4TIou":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","./CheckOutBox":"4TIou"}],"4TIou":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$4550 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
 var prevRefreshReg = window.$RefreshReg$;
 var prevRefreshSig = window.$RefreshSig$;
