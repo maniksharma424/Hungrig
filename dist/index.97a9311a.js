8705fe124f603dbf40c56c0c08fe012b09f4f470
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-02-24 16:10:16 +0530

    Style:Responsive

--- dist/index.97a9311a.js
+++ dist/index.97a9311a.js
@@ -36742,8 +36742,8 @@ var _reselect = require("reselect");
 // src/getDefaultMiddleware.ts
 var _reduxThunk = require("redux-thunk");
 var _reduxThunkDefault = parcelHelpers.interopDefault(_reduxThunk);
-var process = require("91da2a0b3b42edcf");
 var global = arguments[3];
+var process = require("91da2a0b3b42edcf");
 var __extends = undefined && undefined.__extends || function() {
     var extendStatics = function(d, b) {
         extendStatics = Object.setPrototypeOf || ({
